84473e49e14e80a3bada33cf80d91c716f107611
JohnJim0816 <johnjim0816@gmail.com>
2021-03-13 14:09:28 +0800

    update

--- DQN/main.py
+++ DQN/main.py
@@ -5,7 +5,7 @@
 @Email: johnjim0816@gmail.com
 @Date: 2020-06-12 00:48:57
 @LastEditor: John
-LastEditTime: 2021-03-13 14:04:07
+LastEditTime: 2021-03-13 14:09:22
 @Discription: 
 @Environment: python 3.7.7
 '''
@@ -78,44 +78,6 @@ def train(cfg,env,agent):
             ma_rewards.append(ep_reward)   
     print('Complete training！')
     return rewards,ma_rewards
-    
-
-def eval(cfg, saved_model_path = SAVED_MODEL_PATH):
-    print('start to eval ! \n')
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # 检测gpu
-    env = gym.make('CartPole-v0').unwrapped # 可google为什么unwrapped gym，此处一般不需要
-    env.seed(1) # 设置env随机种子
-    n_states = env.observation_space.shape[0]
-    n_actions = env.action_space.n
-    agent = DQN(n_states=n_states, n_actions=n_actions, device="cpu", gamma=cfg.gamma, epsilon_start=cfg.epsilon_start,
-                epsilon_end=cfg.epsilon_end, epsilon_decay=cfg.epsilon_decay, lr=cfg.lr, memory_capacity=cfg.memory_capacity, batch_size=cfg.batch_size)
-    agent.load_model(saved_model_path+'checkpoint.pth')
-    rewards = []
-    ma_rewards = []
-    ep_steps = []
-    for i_episode in range(1, cfg.eval_eps+1):
-        state = env.reset()  # reset环境状态
-        ep_reward = 0
-        for i_step in range(1, cfg.eval_steps+1):
-            action = agent.choose_action(state,train=False)  # 根据当前环境state选择action
-            next_state, reward, done, _ = env.step(action)  # 更新环境参数
-            ep_reward += reward
-            state = next_state  # 跳转到下一个状态
-            if done:
-                break
-        print('Episode:', i_episode, ' Reward: %i' %
-              int(ep_reward), 'n_steps:', i_step, 'done: ', done)
-        ep_steps.append(i_step)
-        rewards.append(ep_reward)
-        # 计算滑动窗口的reward
-        if i_episode == 1:
-            ma_rewards.append(ep_reward)
-        else:
-            ma_rewards.append(
-                0.9*ma_rewards[-1]+0.1*ep_reward)
-    '''存储reward等相关结果'''
-    save_results(rewards,ma_rewards,ep_steps,tag='eval',result_path=RESULT_PATH)
-    print('Complete evaling！')
 
 if __name__ == "__main__":
     cfg = DQNConfig()
