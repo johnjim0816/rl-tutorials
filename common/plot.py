27dc5cc462ca7fc8300d9c7d8478cc972f991592
JohnJim0816 <johnjim0816@gmail.com>
2021-03-13 11:32:33 +0800

    update

--- common/plot.py
+++ common/plot.py
@@ -5,21 +5,19 @@ Author: John
 Email: johnjim0816@gmail.com
 Date: 2020-10-07 20:57:11
 LastEditor: John
-LastEditTime: 2021-03-12 16:52:03
+LastEditTime: 2021-03-13 11:31:49
 Discription: 
 Environment: 
 '''
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import os 
 def plot_rewards(rewards,ma_rewards,tag="train",algo = "On-Policy First-Visit MC Control",path='./'):
     sns.set()
     plt.title("average learning curve of {}".format(algo))
     plt.xlabel('epsiodes')
     plt.plot(rewards,label='rewards')
     plt.plot(ma_rewards,label='moving average rewards')
-    plt.savefig(path+"rewards_curve_{}".format(tag))
     plt.legend()
+    plt.savefig(path+"rewards_curve_{}".format(tag))
     plt.show()
-    
+   
