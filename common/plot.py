35abfff64b90636aab000ddfb7c657717f33fa9c
JohnJim0816 <johnjim0816@gmail.com>
2021-04-28 10:19:18 +0800

    update PPO

--- common/plot.py
+++ common/plot.py
@@ -5,32 +5,32 @@ Author: John
 Email: johnjim0816@gmail.com
 Date: 2020-10-07 20:57:11
 LastEditor: John
-LastEditTime: 2021-04-28 09:57:53
+LastEditTime: 2021-04-28 10:13:21
 Discription: 
 Environment: 
 '''
 import matplotlib.pyplot as plt
 import seaborn as sns
-# def plot_rewards(rewards,ma_rewards,tag="train",env='CartPole-v0',algo = "DQN",save=True,path='./'):
-#     sns.set()
-#     plt.title("average learning curve of {} for {}".format(algo,env))
-#     plt.xlabel('epsiodes')
-#     plt.plot(rewards,label='rewards')
-#     plt.plot(ma_rewards,label='moving average rewards')
-#     plt.legend()
-#     if save:
-#         plt.savefig(path+"rewards_curve_{}".format(tag))
-#     plt.show()
-def plot_rewards(dic,tag="train",env='CartPole-v0',algo = "DQN",save=True,path='./'):
+def plot_rewards(rewards,ma_rewards,tag="train",env='CartPole-v0',algo = "DQN",save=True,path='./'):
     sns.set()
     plt.title("average learning curve of {} for {}".format(algo,env))
     plt.xlabel('epsiodes')
-    for key, value in dic.items():
-        plt.plot(value,label=key)
+    plt.plot(rewards,label='rewards')
+    plt.plot(ma_rewards,label='ma rewards')
     plt.legend()
     if save:
-        plt.savefig(path+algo+"_rewards_curve_{}".format(tag))
+        plt.savefig(path+"rewards_curve_{}".format(tag))
     plt.show()
+# def plot_rewards(dic,tag="train",env='CartPole-v0',algo = "DQN",save=True,path='./'):
+#     sns.set()
+#     plt.title("average learning curve of {} for {}".format(algo,env))
+#     plt.xlabel('epsiodes')
+#     for key, value in dic.items():
+#         plt.plot(value,label=key)
+#     plt.legend()
+#     if save:
+#         plt.savefig(path+algo+"_rewards_curve_{}".format(tag))
+#     plt.show()
 def plot_losses(losses,algo = "DQN",save=True,path='./'):
     sns.set()
     plt.title("loss curve of {}".format(algo))
