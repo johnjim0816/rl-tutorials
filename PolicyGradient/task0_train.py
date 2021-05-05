072335016da4efb9ee6664a5e5744ee1deb9dfc4
JohnJim0816 <johnjim0816@gmail.com>
2021-05-05 17:36:17 +0800

    update

--- PolicyGradient/task0_train.py
+++ PolicyGradient/task0_train.py
@@ -3,105 +3,122 @@
 '''
 Author: John
 Email: johnjim0816@gmail.com
-Date: 2021-03-11 14:26:44
+Date: 2020-11-22 23:21:53
 LastEditor: John
-LastEditTime: 2021-05-05 16:59:43
+LastEditTime: 2021-05-05 17:35:20
 Discription: 
 Environment: 
 '''
-
 import sys,os
 curr_path = os.path.dirname(__file__)
 parent_path = os.path.dirname(curr_path)
 sys.path.append(parent_path)  # add current terminal path to sys.path
 
+import gym
 import torch
 import datetime
+from itertools import count
 
-from common.utils import save_results,make_dir
+from PolicyGradient.agent import PolicyGradient
 from common.plot import plot_rewards
-from MonteCarlo.agent import FisrtVisitMC
-from envs.racetrack_env import RacetrackEnv
+from common.utils import save_results,make_dir
 
 curr_time = datetime.datetime.now().strftime(
     "%Y%m%d-%H%M%S")  # obtain current time
 
-class MCConfig:
+class PGConfig:
     def __init__(self):
-        self.algo = "MC"  # name of algo
-        self.env = 'Racetrack'
+        self.algo = "PolicyGradient"  # name of algo
+        self.env = 'CartPole-v0'
         self.result_path = curr_path+"/outputs/" + self.env + \
             '/'+curr_time+'/results/'  # path to save results
         self.model_path = curr_path+"/outputs/" + self.env + \
             '/'+curr_time+'/models/'  # path to save models
-        # epsilon: The probability to select a random action .
-        self.epsilon = 0.15
-        self.gamma = 0.9  # gamma: Gamma discount factor.
-        self.train_eps = 200
+        self.train_eps = 300 # 训练的episode数目
+        self.eval_eps = 50
+        self.batch_size = 8
+        self.lr = 0.01 # learning rate
+        self.gamma = 0.99
+        self.hidden_dim = 36 # dimmension of hidden layer
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")  # check gpu
 
-def env_agent_config(cfg,seed=1):
-    env = RacetrackEnv()
-    action_dim = 9
-    agent = FisrtVisitMC(action_dim, cfg)
-    return env,agent
-    
-def train(cfg, env, agent):
-    print('Start to eval !')
-    print(f'Env:{cfg.env}, Algorithm:{cfg.algo}, Device:{cfg.device}')
-    rewards = []
-    ma_rewards = []  # moving average rewards
-    for i_ep in range(cfg.train_eps):
-        state = env.reset()
-        ep_reward = 0
-        one_ep_transition = []
-        while True:
-            action = agent.choose_action(state)
-            next_state, reward, done = env.step(action)
-            ep_reward += reward
-            one_ep_transition.append((state, action, reward))
-            state = next_state
-            if done:
-                break
-        rewards.append(ep_reward)
-        if ma_rewards:
-            ma_rewards.append(ma_rewards[-1]*0.9+ep_reward*0.1)
-        else:
-            ma_rewards.append(ep_reward)
-        agent.update(one_ep_transition)
-        if (i_ep+1) % 10 == 0:
-            print(f"Episode:{i_ep+1}/{cfg.train_eps}: Reward:{ep_reward}")
-    print('Complete training！')
-    return rewards, ma_rewards
 
-def eval(cfg, env, agent):
+def env_agent_config(cfg,seed=1):
+    env = gym.make(cfg.env)  
+    env.seed(seed)
+    state_dim = env.observation_space.shape[0]
+    agent = PolicyGradient(state_dim,cfg)
+    return env,agent
+
+def train(cfg,env,agent):
+    print('Start to eval !')
+    print(f'Env:{cfg.env}, Algorithm:{cfg.algo}, Device:{cfg.device}')
+    state_pool = [] # 存放每batch_size个episode的state序列
+    action_pool = []
+    reward_pool = [] 
+    rewards = []
+    ma_rewards = []
+    for i_episode in range(cfg.train_eps):
+        state = env.reset()
+        ep_reward = 0
+        for _ in count():
+            action = agent.choose_action(state) # 根据当前环境state选择action
+            next_state, reward, done, _ = env.step(action)
+            ep_reward += reward
+            if done:
+                reward = 0
+            state_pool.append(state)
+            action_pool.append(float(action))
+            reward_pool.append(reward)
+            state = next_state
+            if done:
+                print('Episode:', i_episode, ' Reward:',  ep_reward)
+                break
+        if i_episode > 0 and i_episode % cfg.batch_size == 0:
+            agent.update(reward_pool,state_pool,action_pool)
+            state_pool = [] # 每个episode的state
+            action_pool = []
+            reward_pool = []
+        rewards.append(ep_reward)
+        if ma_rewards:
+            ma_rewards.append(
+                0.9*ma_rewards[-1]+0.1*ep_reward)
+        else:
+            ma_rewards.append(ep_reward)
+    print('complete training！')
+    return rewards, ma_rewards
+            
+
+def eval(cfg,env,agent):
     print('Start to eval !')
     print(f'Env:{cfg.env}, Algorithm:{cfg.algo}, Device:{cfg.device}')
     rewards = []
-    ma_rewards = []  # moving average rewards
-    for i_ep in range(cfg.train_eps):
+    ma_rewards = []
+    for i_episode in range(cfg.eval_eps):
         state = env.reset()
         ep_reward = 0
-        while True:
-            action = agent.choose_action(state)
-            next_state, reward, done = env.step(action)
+        for _ in count():
+            action = agent.choose_action(state) # 根据当前环境state选择action
+            next_state, reward, done, _ = env.step(action)
             ep_reward += reward
+            if done:
+                reward = 0
             state = next_state
             if done:
+                print('Episode:', i_episode, ' Reward:',  ep_reward)
                 break
         rewards.append(ep_reward)
         if ma_rewards:
-            ma_rewards.append(ma_rewards[-1]*0.9+ep_reward*0.1)
+            ma_rewards.append(
+                0.9*ma_rewards[-1]+0.1*ep_reward)
         else:
             ma_rewards.append(ep_reward)
-        if (i_ep+1) % 10 == 0:
-            print(f"Episode:{i_ep+1}/{cfg.train_eps}: Reward:{ep_reward}")
+    print('complete evaling！')
     return rewards, ma_rewards
-    
 if __name__ == "__main__":
-    cfg = MCConfig()
-    
+    cfg = PGConfig()
+
      # train
     env,agent = env_agent_config(cfg,seed=1)
     rewards, ma_rewards = train(cfg, env, agent)
@@ -116,3 +133,4 @@ if __name__ == "__main__":
     rewards,ma_rewards = eval(cfg,env,agent)
     save_results(rewards,ma_rewards,tag='eval',path=cfg.result_path)
     plot_rewards(rewards,ma_rewards,tag="eval",env=cfg.env,algo = cfg.algo,path=cfg.result_path)
+
