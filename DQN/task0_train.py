fdb6f51521776a11e2105df40440d32b982743fc
johnjim0816 <johnjim0816@gmail.com>
2021-11-17 18:29:15 +0800

    update DQN

--- DQN/task0_train.py
+++ DQN/task0_train.py
@@ -12,7 +12,7 @@ LastEditTime: 2021-09-15 15:34:13
 import sys,os
 curr_path = os.path.dirname(os.path.abspath(__file__)) # 当前文件所在绝对路径
 parent_path = os.path.dirname(curr_path) # 父路径
-sys.path.append(parent_path) # 添加路径到系统路径sys.path
+sys.path.append(parent_path) # 添加路径到系统路径
 
 import gym
 import torch
