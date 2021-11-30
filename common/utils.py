bf60fa57e0e896c0921ee59ffae009c256c09999
johnjim0816 <johnjim0816@gmail.com>
2021-11-30 18:38:01 +0800

    add  plot_rewards_cn

--- common/utils.py
+++ common/utils.py
@@ -5,7 +5,7 @@ Author: John
 Email: johnjim0816@gmail.com
 Date: 2021-03-12 16:02:24
 LastEditor: John
-LastEditTime: 2021-09-11 21:48:49
+LastEditTime: 2021-11-30 18:37:03
 Discription: 
 Environment: 
 '''
@@ -15,41 +15,68 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_rewards(rewards,ma_rewards,plot_cfg,tag='train'):
-    sns.set() 
-    plt.figure() # 创建一个图形实例，方便同时多画几个图
-    plt.title("learning curve on {} of {} for {}".format(plot_cfg.device, plot_cfg.algo, plot_cfg.env_name))
+# from matplotlib.font_manager import FontProperties # 导入字体模块
+
+# def chinese_font():
+#     ''' 设置中文字体
+#     '''
+#     return FontProperties(fname='/System/Library/Fonts/STHeiti Light.ttc',size=15)  # fname系统字体路径，此处是mac的
+# def plot_rewards_cn(rewards,ma_rewards,plot_cfg,tag='train'):
+#     ''' 中文画图
+#     '''
+#     sns.set()
+#     plt.figure()
+#     plt.title(u"{}环境下{}算法的学习曲线".format(env,algo),fontproperties=chinese_font())
+#     plt.xlabel(u'回合数',fontproperties=chinese_font())
+#     plt.plot(rewards)
+#     plt.plot(ma_rewards)
+#     plt.legend((u'奖励',u'滑动平均奖励',),loc="best",prop=chinese_font())
+#     if save:
+#         plt.savefig(path+f"{tag}_rewards_curve_cn")
+#     # plt.show()
+
+
+def plot_rewards(rewards, ma_rewards, plot_cfg, tag='train'):
+    sns.set()
+    plt.figure()  # 创建一个图形实例，方便同时多画几个图
+    plt.title("learning curve on {} of {} for {}".format(
+        plot_cfg.device, plot_cfg.algo, plot_cfg.env_name))
     plt.xlabel('epsiodes')
-    plt.plot(rewards,label='rewards')
-    plt.plot(ma_rewards,label='ma rewards')
+    plt.plot(rewards, label='rewards')
+    plt.plot(ma_rewards, label='ma rewards')
     plt.legend()
     if plot_cfg.save:
         plt.savefig(plot_cfg.result_path+"{}_rewards_curve".format(tag))
     plt.show()
 
-def plot_losses(losses,algo = "DQN",save=True,path='./'):
+
+def plot_losses(losses, algo="DQN", save=True, path='./'):
     sns.set()
     plt.figure()
     plt.title("loss curve of {}".format(algo))
     plt.xlabel('epsiodes')
-    plt.plot(losses,label='rewards')
+    plt.plot(losses, label='rewards')
     plt.legend()
     if save:
         plt.savefig(path+"losses_curve")
     plt.show()
 
-def save_results(rewards,ma_rewards,tag='train',path='./results'):
+
+def save_results(rewards, ma_rewards, tag='train', path='./results'):
     ''' 保存奖励
     '''
     np.save(path+'{}_rewards.npy'.format(tag), rewards)
     np.save(path+'{}_ma_rewards.npy'.format(tag), ma_rewards)
     print('结果保存完毕！')
 
+
 def make_dir(*paths):
     ''' 创建文件夹
     '''
     for path in paths:
         Path(path).mkdir(parents=True, exist_ok=True)
+
+
 def del_empty_dir(*paths):
     ''' 删除目录下所有空文件夹
     '''
