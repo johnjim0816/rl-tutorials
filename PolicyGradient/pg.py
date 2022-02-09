2ff7482aa697d6980a2da14fa6b02a1f7cfaee2b
johnjim0816 <johnjim0816@gmail.com>
2022-02-10 06:14:15 +0800

    update policygradient

--- PolicyGradient/pg.py
+++ PolicyGradient/pg.py
@@ -5,16 +5,36 @@ Author: John
 Email: johnjim0816@gmail.com
 Date: 2020-11-22 23:27:44
 LastEditor: John
-LastEditTime: 2021-10-16 00:43:52
+LastEditTime: 2022-02-10 01:25:27
 Discription: 
 Environment: 
 '''
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.distributions import Bernoulli
 from torch.autograd import Variable
 import numpy as np
-from PolicyGradient.model import MLP
 
+class MLP(nn.Module):
+    
+    ''' 多层感知机
+        输入：state维度
+        输出：概率
+    '''
+    def __init__(self,input_dim,hidden_dim = 36):
+        super(MLP, self).__init__()
+        # 24和36为hidden layer的层数，可根据input_dim, action_dim的情况来改变
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim,hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, 1)  # Prob of Left
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
+        return x
+        
 class PolicyGradient:
     
     def __init__(self, state_dim,cfg):
