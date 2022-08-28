09992ab77c500cf1548e673839b3982259d56ce4
johnjim0816 <39483938+johnjim0816@users.noreply.github.com>
2022-08-28 22:08:23 +0800

    update DQN

--- codes/scripts/DQN_CartPole-v1.sh
+++ codes/scripts/DQN_CartPole-v1.sh
@@ -1,6 +1,4 @@
-'''
-run DQN on CartPole-v1, not finished yet
-'''
+# run DQN on CartPole-v1, not finished yet
 # source conda, if you are already in proper conda environment, then comment the codes util "conda activate easyrl" 
 if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
     echo "source file at ~/anaconda3/etc/profile.d/conda.sh"
@@ -13,4 +11,4 @@ else
 fi
 conda activate easyrl # easyrl here can be changed to another name of conda env that you have created
 codes_dir=$(dirname $(dirname $(readlink -f "$0"))) # "codes" path
-python $codes_dir/DQN/main.py --env_name CartPole-v1 --train_eps 500 --epsilon_decay 1000 --memory_capacity 200000 --batch_size 128 --device cuda
+python $codes_dir/DQN/main.py --env_name CartPole-v1 --train_eps 2000 --gamma 0.99 --epsilon_decay 6000 --lr 0.00001 --memory_capacity 200000 --batch_size 64 --device cuda
