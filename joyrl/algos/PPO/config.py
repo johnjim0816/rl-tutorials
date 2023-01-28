6e3b0873fb0c750f1215219f32edfa2ca5c406c3
scchy <hyscc1994@foxmail.com>
2023-01-28 20:59:22 +0800

    reset

--- joyrl/algos/PPO/config.py
+++ joyrl/algos/PPO/config.py
@@ -15,23 +15,3 @@ class AlgoConfig:
         self.entropy_coef = 0.01 # entropy coefficient
         self.actor_hidden_dim = 256 # hidden dimension for actor
         self.critic_hidden_dim = 256 # hidden dimension for critic
-        self.model_dir = os.getcwd()
-        # env setting
-        self.continuous = True # continuous action space
-        self.seed = 2023
-        self.new_step_api = True
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.max_steps = 500
-        
-        # PPO kwargs
-        self.k_epochs = 10  # update policy for K epochs
-        self.policy_clip = 0.2
-        self.train_batch_size = 1024 # ppo train batch size
-        self.sgd_batch_size = 512 # sgd batch size
-        self.gae_lambda = 0.9
-        self.gamma = 0.9 # discount factor
-        self.actor_nums = 3
-        self.actor_lr = 1e-4 # learning rate for actor
-        self.critic_lr = 5e-3 # learning rate for critic
-        self.eps_clip = 0.2 # clip parameter for PPO
-        
