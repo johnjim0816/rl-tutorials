bec3e82356229fa33a5dd7d65ba9d3a19ca5972b
wangzhongren <wangzhongren@sjtu.edu.cn>
2023-02-23 18:56:43 +0800

    update the SAC

--- joyrl/algos/SAC/config.py
+++ joyrl/algos/SAC/config.py
@@ -1,13 +1,13 @@
 class AlgoConfig:
     def __init__(self) -> None:
         self.policy_type = 'Gaussian' # policy type
-        self.lr = 3e-4 # learning rate
+        self.lr = 1e-3 # learning rate # 3e-4
         self.gamma = 0.99 # discount factor
         self.tau = 0.005 # soft update factor
-        self.alpha = 0.2 # Temperature parameter α determines the relative importance of the entropy term against the reward
+        self.alpha = 0.1 # Temperature parameter α determines the relative importance of the entropy term against the reward # 0.1
         self.automatic_entropy_tuning = False # automatically adjust α
-        self.batch_size = 256 # batch size
-        self.hidden_dim = 256 # hidden dimension
+        self.batch_size = 64 # batch size # 256
+        self.hidden_dim = 64 # hidden dimension # 256
         self.n_epochs = 1 # number of epochs
         self.start_steps = 10000 # number of random steps for exploration
         self.target_update_fre = 1 # interval for updating the target network
